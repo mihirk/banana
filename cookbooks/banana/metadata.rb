4dfc027de05d63935363cd8abf931a7256097ad9
Mihir <mihir.khatwani@gmail.com>
2014-01-08 22:37:00 +0530

    Initial with cookbook

---
+++ cookbooks/banana/metadata.rb
@@ -0,0 +1,7 @@
+name             'banana'
+maintainer       'ThoughtWorks'
+maintainer_email 'mihir.khatwani@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures banana'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/banana/recipes/default.rb
@@ -0,0 +1,19 @@
+remote_file "/tmp/epel-release-6-8.noarch.rpm" do
+      action :create_if_missing
+      source "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+      not_if "rpm -qa | grep -qx 'epel'"
+end
+
+rpm_package "/tmp/epel-release-6-8.noarch.rpm" do
+	action :install
+end
+
+package "python" do
+	version	"2.6.6-51.el6"
+	action	:install
+end
+
+package "python-pip" do
+	action	:install
+end
+
